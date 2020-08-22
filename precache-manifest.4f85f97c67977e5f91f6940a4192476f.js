2a701d5417281a132c81abddd2c18bdef46aad93
TUanphan nam <phantuanw.o.t@gmail.com>
2020-08-22 22:48:11 +0700

    Updates

---
+++ precache-manifest.4f85f97c67977e5f91f6940a4192476f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8417bcb1aadc210098fb",
+    "url": "/Deplou_thoi/static/css/main.ee4291fd.chunk.css"
+  },
+  {
+    "revision": "8417bcb1aadc210098fb",
+    "url": "/Deplou_thoi/static/js/main.c9972b5e.chunk.js"
+  },
+  {
+    "revision": "2a35127d0541aaa23d92",
+    "url": "/Deplou_thoi/static/js/runtime~main.84eb7071.js"
+  },
+  {
+    "revision": "76bad2b7122aa80ab7da",
+    "url": "/Deplou_thoi/static/css/2.a8b0f192.chunk.css"
+  },
+  {
+    "revision": "76bad2b7122aa80ab7da",
+    "url": "/Deplou_thoi/static/js/2.4d7e1338.chunk.js"
+  },
+  {
+    "revision": "0a5032eeae0b54bc64b163ef08bc843e",
+    "url": "/Deplou_thoi/index.html"
+  }
+];
